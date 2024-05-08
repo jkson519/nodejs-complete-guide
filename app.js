6333a309e02bd54ee9e7272351a077acfe677fbc
SonJeKeun <b24022.tdc.hanmacgroup@gmail.com>
2024-05-08 16:08:06 +0900

    32강 진행중

--- app.js
+++ app.js
@@ -10,8 +10,32 @@ const http = require('http');
 // });
 
 // 3. 화살표 함수
+// const server = http.createServer((req, res) => {
+//     console.log(req.url, req.method, req.headers);
+
+//     // 이벤트 루프를 종료해서 프로그램 종료
+//     // process.exit();
+
+//     res.setHeader('Content-Type', 'text/html');
+//     res.write('<html>');
+//     res.write('<head><title>My First Page</title></head>');
+//     // res.write('<body><h1>Hello from my Node.js Server!</h1></body>');
+//     let lang = 'Node.js';
+//     res.write(`<body><h1>Hello from my ${lang} Server!</h1></body>`);
+//     res.write('</html>');
+//     res.end();
+//     // end를 호출한 뒤에 새로 입력을 시도할 경우 'ERR_STREAM_WRITE_AFTER_END' 오류 발생
+// });
+
 const server = http.createServer((req, res) => {
-    console.log(req);
+    const url = req.url;
+    if (url === '/') {
+        res.write('<html>');
+        res.write('<head><title>Enter Message</title></head>');
+        res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button>/form></body>');
+        res.write('</html>');
+        res.end();
+    }
 });
 
 // listen은 Node.js가 스크립트를 바로 종료하지 않고 계속 실행되면서 '듣도록' 하는 역할
