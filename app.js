ebba5eb0b01e776cd3c6b1332a0f8d4b9944e546
SonJeKeun <b24022.tdc.hanmacgroup@gmail.com>
2024-05-09 18:15:55 +0900

    37강 진행중

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const http = require('http');
+const fs = require('fs');
 
 // 1. 별도 함수
 // function rqListener(req, res) {
@@ -29,12 +30,30 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
     const url = req.url;
+    const method = req.method;
+
     if (url === '/') {
         res.write('<html>');
         res.write('<head><title>Enter Message</title></head>');
         res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
         res.write('</html>');
         return res.end();
+    }
+
+    if (url === '/message' && method === 'POST') {
+        const body = [];
+        req.on('data', (chunk) => {
+            body.push(chunk);
+        });
+        return req.on('end', () => {
+            const parsedBody = Buffer.concat(body).toString();
+            const message = parsedBody.split('=')[1];
+            fs.writeFile('message.txt', message, err => {
+                res.statusCode = 302;
+                res.setHeader('Location', '/');
+                return res.end();
+            });
+        });
     }
 
     res.setHeader('Content-Type', 'text/html');
