7ebccdef8b73bf2840ad4b2053f16da114d61436
SonJeKeun <b24022.tdc.hanmacgroup@gmail.com>
2024-05-13 16:00:07 +0900

    49강까지 완료

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const http = require('http');
-const fs = require('fs');
+
+const routes = require('./routes');
 
 // 1. 별도 함수
 // function rqListener(req, res) {
@@ -28,42 +29,7 @@ const fs = require('fs');
 //     // end를 호출한 뒤에 새로 입력을 시도할 경우 'ERR_STREAM_WRITE_AFTER_END' 오류 발생
 // });
 
-const server = http.createServer((req, res) => {
-    const url = req.url;
-    const method = req.method;
-
-    if (url === '/') {
-        res.write('<html>');
-        res.write('<head><title>Enter Message</title></head>');
-        res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
-        res.write('</html>');
-        return res.end();
-    }
-
-    if (url === '/message' && method === 'POST') {
-        const body = [];
-        req.on('data', (chunk) => {
-            body.push(chunk);
-        });
-        return req.on('end', () => {
-            const parsedBody = Buffer.concat(body).toString();
-            const message = parsedBody.split('=')[1];
-            fs.writeFile('message.txt', message, err => {
-                res.statusCode = 302;
-                res.setHeader('Location', '/');
-                return res.end();
-            });
-        });
-    }
-
-    res.setHeader('Content-Type', 'text/html');
-    res.write('<html>');
-    res.write('<head><title>My First Page</title></head>');
-    let lang = 'Node.js';
-    res.write(`<body><h1>Hello from my ${lang} Server!</h1></body>`);
-    res.write('</html>');
-    res.end();
-});
+const server = http.createServer(routes.handler);
 
 // listen은 Node.js가 스크립트를 바로 종료하지 않고 계속 실행되면서 '듣도록' 하는 역할
-server.listen(3000);
+server.listen(3001);
